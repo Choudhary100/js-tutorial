450c7f3cd63f3a48aa1faae9d178c6e182719ad6
Choudhary100 <145636410+Choudhary100@users.noreply.github.com>
2023-09-22 04:34:05 +0000

    container configuration

---
+++ 01_jshindi/text.js
@@ -0,0 +1,11 @@
+console.log("Rajeev");
+
+const username="Rajeev"
+let accountemail="rajeev890@gmail.com"
+var password="1234"
+city="rajisthan"
+accountemail="rajeevpro.374@gmail.co.in"
+password="888888"
+city="bihar"
+// username="rani"
+console.table([accountemail,city,password,username])
