9292a4998762f9cd9a6b248dc09f29fcdc5333dd
Choudhary100 <145636410+Choudhary100@users.noreply.github.com>
2023-09-27 14:27:27 +0000

    Conversion of data types

---
+++ 01_jshindi/basic_01.js
@@ -0,0 +1,43 @@
+// let score= "33"
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber=Number(score)  // to make string value into number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//if we pass the value into Number() then these are the outputs
+//"33"=> 33
+//"33abc"=>NaN
+//true=> 1 , False=> 0.
+
+// let isLoggedIn=1
+
+// let booleanIsLoggedIn= Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let someNumber=33
+// let stringnumber=String(someNumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+
+// Conversion of string to number..
+
+// let goal="23";
+// console.log(typeof goal);
+
+// let valueInNumber=Number(goal)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+
+//Conversion of number into string
+
+let anynumber=90
+console.log(typeof anynumber);
+let StringNumber= String(anynumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
